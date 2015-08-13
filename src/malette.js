76cfeca9f8c6b4b2968ed33c4f4b819007389e05
brendan <brendan.heberton@gmail.com>
2015-08-13 14:04:49 -0600

    adding colors arrays to colors module

--- src/malette.js
+++ src/malette.js
@@ -2,7 +2,7 @@
 //! version : 1.00
 //!license : MIT 
 
-import { dojoColorToRgba, rgbaToDojoColor } from './colors.js';
+import { dojoColorToRgba, rgbaToDojoColor, themeColors, colorSwatches } from './colors.js';
 import { classify } from './classify.js';
 import { stringToDom } from './string-to-dom.js';
 
@@ -181,15 +181,7 @@ class Malette {
   */ 
   _addColors (el, selectedColor) {
     var swatch;
-    var colors = [
-      'rgb(255,255,255)','rgb(240,240,240)','rgb(217,217,217)','rgb(189,189,189)','rgb(150,150,150)','rgb(115,115,115)','rgb(82,82,82)','rgb(37,37,37)',
-      'rgb(247,252,253)','rgb(224,236,244)','rgb(191,211,230)','rgb(158,188,218)','rgb(140,150,198)','rgb(140,107,177)','rgb(136,65,157)','rgb(110,1,107)',
-      'rgb(255,247,243)','rgb(253,224,221)','rgb(252,197,192)','rgb(250,159,181)','rgb(247,104,161)','rgb(221,52,151)','rgb(174,1,126)','rgb(122,1,119)',
-      'rgb(255,255,204)','rgb(255,237,160)','rgb(254,217,118)','rgb(254,178,76)','rgb(253,141,60)','rgb(252,78,42)','rgb(227,26,28)','rgb(177,0,38)',
-      'rgb(255,255,217)','rgb(237,248,177)','rgb(199,233,180)','rgb(127,205,187)','rgb(65,182,196)','rgb(29,145,192)','rgb(34,94,168)','rgb(12,44,132)',
-      'rgb(247,252,240)','rgb(224,243,219)','rgb(204,235,197)','rgb(168,221,181)','rgb(123,204,196)','rgb(78,179,211)','rgb(43,140,190)','rgb(8,88,158)',
-      'rgb(247,252,253)','rgb(229,245,249)','rgb(204,236,230)','rgb(153,216,201)','rgb(102,194,164)','rgb(65,174,118)','rgb(35,139,69)','rgb(0,88,36)'
-    ];
+    var colors = colorSwatches; 
 
     var colorPalette = this._createElement('div', el, 'malette-color-palette', '', '');
 
@@ -214,23 +206,21 @@ class Malette {
     var self = this;
     var swatch;
 
-    var select = this._createAttributeSelect();
+    var select = this._createAttributeSelect('malette-attr-select');
 
-    this.themeColors = [
-      ['rgb(255,255,255)','rgb(240,240,240)','rgb(217,217,217)','rgb(189,189,189)','rgb(150,150,150)','rgb(115,115,115)','rgb(82,82,82)','rgb(37,37,37)'],
-      ['rgb(255,255,217)','rgb(237,248,177)','rgb(199,233,180)','rgb(127,205,187)','rgb(65,182,196)','rgb(29,145,192)','rgb(34,94,168)','rgb(12,44,132)'],
-      ['rgb(255,255,229)','rgb(247,252,185)','rgb(217,240,163)','rgb(173,221,142)','rgb(120,198,121)','rgb(65,171,93)','rgb(35,132,67)','rgb(0,90,50)'],
-      ['rgb(255,247,251)','rgb(236,226,240)','rgb(208,209,230)','rgb(166,189,219)','rgb(103,169,207)','rgb(54,144,192)','rgb(2,129,138)','rgb(1,100,80)'],
-      ['rgb(247,252,253)','rgb(224,236,244)','rgb(191,211,230)','rgb(158,188,218)','rgb(140,150,198)','rgb(140,107,177)','rgb(136,65,157)','rgb(110,1,107)'],
-      ['rgb(255,255,204)','rgb(255,237,160)','rgb(254,217,118)','rgb(254,178,76)','rgb(253,141,60)','rgb(252,78,42)','rgb(227,26,28)','rgb(177,0,38)'],
-      ['rgb(255,245,235)','rgb(254,230,206)','rgb(253,208,162)','rgb(253,174,107)','rgb(253,141,60)','rgb(241,105,19)','rgb(217,72,1)','rgb(140,45,4)']
-    ];
+    this.themeColors = themeColors;
 
-    var colorPaletteContainer = this._createElement('div', el, 'malette-theme-palette', '', '');
+    var template = `
+      <div id='malette-theme-palette'>
+        <div id='malette-theme-palette-inner'></div>
+      </div>
+    `
+    el.appendChild(stringToDom(template));
 
-    colorPaletteContainer.appendChild( select ).id = 'malette-attr-select';
-  
-    var colorPalette = this._createElement('div', colorPaletteContainer, 'malette-theme-palette-inner', '', '');    
+    var palette = document.getElementById( 'malette-theme-palette');
+    var colorPalette = document.getElementById( 'malette-theme-palette-inner' );
+
+    palette.insertBefore(select, colorPalette);
 
     this.themeColors.forEach(function(colors, i) {
       var row = self._createElement('div', colorPalette, 'malette-theme-row-'+i, '', 'malette-theme-row');
@@ -448,8 +438,10 @@ class Malette {
   * Requires this.options.fields be defined; 
   *
   */
-  _createAttributeSelect () {
+  _createAttributeSelect (id) {
     var select = document.createElement('select');
+    if ( id ) select.id = id;
+
     for (var i = 0; i < this.options.fields.length; i++) { 
       if ( this.options.fields[i].type === 'esriFieldTypeDouble' || this.options.fields[i].type === 'esriFieldTypeInteger' || this.options.fields[i].type === 'esriFieldTypeSingle' || this.options.fields[i].type === 'esriFieldTypeSmallInteger' ) {
         if ( this.options.fields[i].statistics && this.options.fields[i].statistics.max ) {
